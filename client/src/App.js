8b91dc3d81d6cd70c1e262acbd29cf8fab72c786
HuynhTonyy <107403128+HuynhTonyy@users.noreply.github.com>
2023-11-10 17:20:55 +0700

    Some field manager
    dont disappear pls

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import {Landing} from "./views"
+import FieldManage from './views/FieldManage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}/>
+        <Route path="/FieldManage" element={<FieldManage />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/views/FieldManage.jsx
@@ -0,0 +1,294 @@
+
+import "../tan-css/FieldManager.css";
+const FieldManage = () =>{
+    return (
+        <div>
+    <div className="qun-l">
+      <div className="landing">
+        <div className="landiing-main">
+          <img
+            className="landing-img2-icon"
+            alt=""
+            src="/landing-img2@2x.png"
+          />
+          <div className="landing-mask" />
+          <div className="get-started">
+            <div
+              className="button"
+              sx={{ width: 300 }}
+              color="primary"
+              variant="contained"
+            />
+            <b className="thm-sn-ngay">Thêm sân ngay</b>
+          </div>
+          <div className="now-manage-your-container">
+            <p className="now-manage-your">{`Now, manage your `}</p>
+            <p className="now-manage-your">fields here.</p>
+          </div>
+          <div className="sign-in">Sign in</div>
+          <b className="sign-in1">Quản lý sân</b>
+          <b className="sign-in2">Quản lý lịch sân</b>
+          <div className="sign-up">
+            <div className="button1" />
+            <b className="sign-up1">Sign up</b>
+          </div>
+          <div className="landiing-main-child" />
+          <img className="landiing-main-item" alt="" src="/rectangle-1.svg" />
+        </div>
+        <div className="qun-l-sn">
+          <div className="nhp-tn-sn-parent">
+            <div className="nhp-tn-sn">
+              <div className="nhp-tn-sn-child" />
+              <div className="tn-sn">Tên sân</div>
+              <div className="tn-sn1">Tên sân:</div>
+            </div>
+            <div className="chn-loi-sn">
+              <div className="chn-loi-sn-child" />
+              <div className="tn-sn">Loại sân</div>
+              <div className="loi-sn1">Loại sân:</div>
+              <img
+                className="expand-arrow-icon"
+                alt=""
+                src="/expand-arrow@2x.png"
+              />
+            </div>
+            <div className="chn-nh">
+              <div className="chn-nh-child" />
+              <div className="nh">Ảnh:</div>
+              <img
+                className="img-box-fill-icon1"
+                alt=""
+                src="/img-box-fill1.svg"
+              />
+            </div>
+            <div className="nhp-gi">
+              <div className="chn-loi-sn-child" />
+              <div className="tn-sn">Giá tiền</div>
+              <div className="n-gi">Đơn giá:</div>
+            </div>
+            <div className="nt-thm">
+              <div className="nt-thm-child" />
+              <div className="thm">Thêm</div>
+            </div>
+            <div className="nt-cp-nht">
+              <div className="nt-thm-child" />
+              <div className="thm">Cập nhật</div>
+            </div>
+            <div className="nt-xa">
+              <div className="nt-thm-child" />
+              <div className="thm">Xóa</div>
+            </div>
+          </div>
+          <div className="danh-sch-sn">
+            <div className="cac-c-s" />
+            <b className="danh-sch-sn1">Danh sách sân</b>
+          </div>
+          <div className="sn">
+            <div className="cac-c-s1" />
+            <img className="sn-child" alt="" src="/rectangle-4323.svg" />
+            <div className="tn-sn-input">
+              <div className="tn-sn-input-child" />
+              <div className="tn-sn2">Tên sân</div>
+            </div>
+            <div className="tn-sn-input1">
+              <div className="tn-sn-input-item" />
+              <div className="tn-sn2">Loại sân</div>
+            </div>
+            <div className="tn-sn-input2">
+              <div className="tn-sn-input-inner" />
+              <div className="tn-sn2">Giá tiền</div>
+            </div>
+            <img className="sn-item" alt="" src="/group-289647.svg" />
+            <img className="sn-inner" alt="" src="/group-289648.svg" />
+            <img className="editbutton-icon" alt="" src="/editbutton.svg" />
+            <img className="editbutton-icon1" alt="" src="/editbutton1.svg" />
+          </div>
+          <div className="sn1">
+            <div className="cac-c-s2" />
+            <div className="tn-sn-input3">
+              <div className="tn-sn-input-child" />
+              <div className="tn-sn2">Tên sân</div>
+            </div>
+            <div className="tn-sn-input4">
+              <div className="tn-sn-input-item" />
+              <div className="tn-sn2">Loại sân</div>
+            </div>
+            <div className="tn-sn-input5">
+              <div className="tn-sn-input-inner" />
+              <div className="tn-sn2">Giá tiền</div>
+            </div>
+            <img className="group-icon" alt="" src="/group-289647.svg" />
+            <img className="sn-child1" alt="" src="/group-289648.svg" />
+            <img className="editbutton-icon2" alt="" src="/editbutton.svg" />
+            <img className="editbutton-icon3" alt="" src="/editbutton1.svg" />
+          </div>
+          <div className="main-advertise-letter">QUẢN LÝ SÂN</div>
+        </div>
+        <img
+          className="screenshot-121-1-icon"
+          alt=""
+          src="/screenshot-121-1@2x.png"
+        />
+        <img className="ball2-icon" alt="" src="/ball2@2x.png" />
+        <img className="football-icon" alt="" src="/football@2x.png" />
+        <div className="nhp-tn-sn-group">
+          <div className="nhp-tn-sn1">
+            <div className="nhp-tn-sn-item" />
+            <div className="tn-khch">Tên khách</div>
+            <div className="tn">{`Tên: `}</div>
+          </div>
+          <div className="nhp-tn-sn2">
+            <div className="nhp-tn-sn-item" />
+            <div className="tn-khch">Tên sân</div>
+            <div className="loi">Tên:</div>
+            <img
+              className="expand-arrow-icon1"
+              alt=""
+              src="/expand-arrow1@2x.png"
+            />
+          </div>
+          <div className="nhp-tn-sn3">
+            <div className="nhp-tn-sn-child1" />
+            <div className="div">14:00 - 17:00</div>
+            <div className="khung-gi">Khung giờ:</div>
+            <img
+              className="expand-arrow-icon2"
+              alt=""
+              src="/expand-arrow2@2x.png"
+            />
+          </div>
+          <div className="nhp-tn-sn4">
+            <div className="nhp-tn-sn-item" />
+            <div className="tn-khch">Loại sân</div>
+            <div className="loi">Loại:</div>
+            <img
+              className="expand-arrow-icon1"
+              alt=""
+              src="/expand-arrow1@2x.png"
+            />
+          </div>
+          <div className="nhp-tn-sn5">
+            <div className="nhp-tn-sn-child3" />
+            <div className="t7-31102023">T7 31/10/2023</div>
+            <div className="ngy">Ngày:</div>
+            <img className="date-range-icon1" alt="" src="/date-range1.svg" />
+          </div>
+          <b className="thng-tin-khch">Thông tin khách hàng</b>
+          <b className="thng-tin-sn">Thông tin sân</b>
+          <div className="chn-loi-sn1">
+            <div className="chn-loi-sn-item" />
+            <div className="s-in-thoi">Số điện thoại</div>
+            <div className="s-in-thoi1">Số điện thoại:</div>
+          </div>
+          <div className="nt-thm1">
+            <div className="nt-thm-item" />
+            <div className="nt-thm-item" />
+            <div className="thm1">Thêm</div>
+          </div>
+          <div className="nt-cp-nht1">
+            <div className="nt-thm-item" />
+            <div className="thm1">Cập nhật</div>
+          </div>
+          <div className="nt-xa1">
+            <div className="nt-thm-item" />
+            <div className="thm1">Xóa</div>
+          </div>
+          <div className="main-advertise-letter1">QUẢN LÝ LỊCH SÂN</div>
+          <div className="nhp-tn-sn6">
+            <div className="nhp-tn-sn-child4" />
+            <div className="c">Có</div>
+            <div className="cho-php-giao">Cho phép giao hữu</div>
+          </div>
+          <div className="group-parent">
+            <div className="rectangle-parent">
+              <div className="group-child" />
+              <div className="group-item" />
+              <div className="group-inner" />
+              <div className="group-child1" />
+              <div className="group-child2" />
+              <div className="group-child3" />
+              <div className="group-child3" />
+              <div className="group-child5" />
+              <b className="b">8:00 - 10:00</b>
+              <b className="b1">8:00 - 10:00</b>
+              <b className="b2">8:00 - 10:00</b>
+              <b className="b3">8:00 - 10:00</b>
+              <b className="b4">8:00 - 10:00</b>
+              <b className="b5">8:00 - 10:00</b>
+              <b className="b6">8:00 - 10:00</b>
+            </div>
+            <div className="ch-thch">
+              <div className="ch-thch-child" />
+              <div className="ch-thch-item" />
+              <div className="ch-thch-inner" />
+              <div className="ellipse-div" />
+              <div className="ch-thch-child1" />
+              <div className="c-nhn-to">Cỏ nhân tạo</div>
+              <div className="trng">Trống</div>
+              <div className="t">Đã đặt</div>
+              <div className="hon-thnh">Hoàn thành</div>
+              <div className="c-t-nhin">Cỏ tự nhiên</div>
+              <div className="ch-thch-child2" />
+            </div>
+            <div className="group-child6" />
+            <div className="ds-sn">
+              <div className="rectangle-group">
+                <div className="group-child7" />
+                <b className="sn-2">Sân 2</b>
+              </div>
+              <div className="rectangle-container">
+                <div className="group-child7" />
+                <b className="sn-2">Sân 4</b>
+              </div>
+              <div className="group-div">
+                <div className="group-child9" />
+                <b className="sn-3">Sân 3</b>
+              </div>
+              <div className="rectangle-parent1">
+                <div className="group-child9" />
+                <b className="sn-3">Sân 5</b>
+              </div>
+              <div className="rectangle-parent2">
+                <div className="group-child11" />
+                <div className="group-child11" />
+                <b className="sn-1">Sân 1</b>
+                <b className="sn-21">Sân 2</b>
+                <b className="sn-41">Sân 4</b>
+              </div>
+            </div>
+            <img className="group-child13" alt="" src="/group-289676.svg" />
+            <img className="group-child14" alt="" src="/group-289677.svg" />
+            <img className="group-child15" alt="" src="/group-289678.svg" />
+            <img className="group-child16" alt="" src="/group-289678.svg" />
+            <img className="group-child17" alt="" src="/group-289678.svg" />
+            <div className="rectangle-parent3">
+              <div className="group-child18" />
+              <div className="div1">4</div>
+              <img className="polygon-icon" alt="" src="/polygon-2.svg" />
+              <img className="group-child19" alt="" src="/polygon-3.svg" />
+              <div className="group-child20" />
+              <div className="thng-10">Tháng 10</div>
+              <div className="vector-parent">
+                <img
+                  className="rectangle-icon"
+                  alt=""
+                  src="/rectangle-429.svg"
+                />
+                <div className="lc-loi-sn">Lọc loại sân</div>
+                <img
+                  className="expand-arrow-icon4"
+                  alt=""
+                  src="/expand-arrow3@2x.png"
+                />
+              </div>
+            </div>
+            <img className="group-child21" alt="" src="/rectangle-444.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+    )
+};
+
+export default FieldManage;
