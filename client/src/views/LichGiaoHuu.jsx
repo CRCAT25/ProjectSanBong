d23256a93e76424149e6c65e67c13614d6880028
TriiTonyy1708 <135950372+TriiTonyy1708@users.noreply.github.com>
2023-11-12 13:22:10 +0700

    LichGiaoHuu

--- client/src/views/LichGiaoHuu.jsx
+++ client/src/views/LichGiaoHuu.jsx
@@ -2,34 +2,71 @@ import "../css/LichGiaoHuu.css"
 const LichGiaoHuu = () =>{
     return (
       <div className="ThamGiaGiaoHU" >
-        <div className="Topic">LỊCH GIAO HỮU</div>
-        <div className="BNgLCh text-center" >
-          <div class="grid grid-cols-12 w-full">
-              <div class="col-span-2 px-5">Tên</div>
-              <div class="col-span-2 px-5">SĐT</div>
-              <div class="col-span-2 px-5">Địa điểm</div>
-              <div class="col-span-2 px-5">Mã sân</div>
-              <div class="col-span-2 px-5">Thời gian</div>
-              <div class="col-span-2 px-5">Xác nhận</div>
+        <div className="Topic mb-3">LỊCH GIAO HỮU</div>
+        <div className="BNgLCh text-center p-3" >
+          <div class="grid grid-cols-7 w-full bg-[#D4D4D4] rounded-[10px]">
+              <div class="col-span-1 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">Tên</div>
+              <div class="col-span-1 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">SĐT</div>
+              <div class="col-span-2 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">Địa điểm</div>
+              <div class="col-span-1 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">Mã sân</div>
+              <div class="col-span-1 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">Thời gian</div>
+              <div class="col-span-1 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">Xác nhận</div>
           </div>
-          
-          {/* <div className="ChiTiTLCh" >
-            <div class="Name">Nguyễn Xuân Lộc</div>
-            <div class="PhoneNumber">1234567890</div>
-            <div class="Location">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
-            <div class="FieldId">Sân 01</div>
-            <div class="DateTime">T7 31/12/2023<br/>19h00 - 21h00 </div>
-            <button class="XacNhN">Tham gia</button>
-          </div>     */}
 
-          <div className="mt-10 grid grid-cols-6 bg-[#379E13] text-center justify-center" >
-            <div className="col-span-1 px-5 justify-center">Nguyễn Xuân Lộc</div>
-            <div class="col-span-1 px-5">1234567890</div>
-            <div class="col-span-1 px-5">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
-            <div class="col-span-1 px-5">Sân 01</div>
-            <div class="col-span-1 px-5">T7 31/12/2023<br/>19h00 - 21h00 </div>
-            <button class="col-span-1 px-5">Tham gia</button>
+
+          <div className="mt-3 rounded-[10px] grid grid-cols-7 bg-[#379E13] text-center justify-center py-5 text-[#fff] text-[20px]" >
+            <div className="col-span-1 px-5 flex flex-col justify-center">Nguyễn Xuân Lộc</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">1234567890</div>
+            <div class="col-span-2 px-5 flex flex-col justify-center text-left">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <div className="relative">
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+            </div>
           </div>  
+
+          <div className="mt-3 rounded-[10px] grid grid-cols-7 bg-[#379E13] text-center justify-center py-5 text-[#fff] text-[20px]" >
+            <div className="col-span-1 px-5 flex flex-col justify-center">Nguyễn Xuân Lộc</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">1234567890</div>
+            <div class="col-span-2 px-5 flex flex-col justify-center text-left">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <div className="relative">
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+            </div>
+          </div>  
+          <div className="mt-3 rounded-[10px] grid grid-cols-7 bg-[#379E13] text-center justify-center py-5 text-[#fff] text-[20px]" >
+            <div className="col-span-1 px-5 flex flex-col justify-center">Nguyễn Xuân Lộc</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">1234567890</div>
+            <div class="col-span-2 px-5 flex flex-col justify-center text-left">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <div className="relative">
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+            </div>
+          </div>  
+          <div className="mt-3 rounded-[10px] grid grid-cols-7 bg-[#379E13] text-center justify-center py-5 text-[#fff] text-[20px]" >
+            <div className="col-span-1 px-5 flex flex-col justify-center">Nguyễn Xuân Lộc</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">1234567890</div>
+            <div class="col-span-2 px-5 flex flex-col justify-center text-left">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <div className="relative">
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+            </div>
+          </div>  
+          <div className="mt-3 rounded-[10px] grid grid-cols-7 bg-[#379E13] text-center justify-center py-5 text-[#fff] text-[20px]" >
+            <div className="col-span-1 px-5 flex flex-col justify-center">Nguyễn Xuân Lộc</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">1234567890</div>
+            <div class="col-span-2 px-5 flex flex-col justify-center text-left">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
+            <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <div className="relative">
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+            </div>
+          </div>  
+
+          
         </div>
     </div>
     )
