75138ed5f9ac8be25bdd6de9393d959b10debfe1
TriiTonyy1708 <135950372+TriiTonyy1708@users.noreply.github.com>
2023-11-12 12:50:54 +0700

    update

--- client/src/views/LichGiaoHuu.jsx
+++ client/src/views/LichGiaoHuu.jsx
@@ -2,33 +2,35 @@ import "../css/LichGiaoHuu.css"
 const LichGiaoHuu = () =>{
     return (
       <div className="ThamGiaGiaoHU" >
-      <div className="BNgLCh" >
-        <img className="OthersSay" />
-        <div className="CacCS"  />
         <div className="Topic">LỊCH GIAO HỮU</div>
-        <div class="Tittle">
-          <div class="TittleBackGround"></div>
-          <div class="TN">Tên</div>
-          <div class="ST">SĐT</div>
-          <div class="AIM">Địa điểm</div>
-          <div class="MSN">Mã sân</div>
-          <div class="ThIGian">Thời gian</div>
-          <div class="XCNhN">Xác nhận</div>
-        </div>
-        <div className="ChiTiTLCh" >
-          <div class="CacCS"></div>
-          <div class="Name">Nguyễn Xuân Lộc</div>
-          <div class="PhoneNumber">1234567890</div>
-          <div class="Location">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
-          <div class="FieldId">Sân 01</div>
-          <div class="DateTime">T7 31/12/2023<br/>19h00 - 21h00 </div>
-          <div class="XacNhN">
-            <div class="Rectangle441"></div>
-            <div class="ThamGia">Tham gia</div>
+        <div className="BNgLCh text-center" >
+          <div class="grid grid-cols-12 w-full">
+              <div class="col-span-2 px-5">Tên</div>
+              <div class="col-span-2 px-5">SĐT</div>
+              <div class="col-span-2 px-5">Địa điểm</div>
+              <div class="col-span-2 px-5">Mã sân</div>
+              <div class="col-span-2 px-5">Thời gian</div>
+              <div class="col-span-2 px-5">Xác nhận</div>
           </div>
+          
+          {/* <div className="ChiTiTLCh" >
+            <div class="Name">Nguyễn Xuân Lộc</div>
+            <div class="PhoneNumber">1234567890</div>
+            <div class="Location">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="FieldId">Sân 01</div>
+            <div class="DateTime">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <button class="XacNhN">Tham gia</button>
+          </div>     */}
+
+          <div className="mt-10 grid grid-cols-6 bg-[#379E13] text-center justify-center" >
+            <div className="col-span-1 px-5 justify-center">Nguyễn Xuân Lộc</div>
+            <div class="col-span-1 px-5">1234567890</div>
+            <div class="col-span-1 px-5">Sân Huy Hoàng<br/>D/c: 3123, Nguyễn xuân khoát, Bình Hưng Hòa, Bình Tân</div>
+            <div class="col-span-1 px-5">Sân 01</div>
+            <div class="col-span-1 px-5">T7 31/12/2023<br/>19h00 - 21h00 </div>
+            <button class="col-span-1 px-5">Tham gia</button>
+          </div>  
         </div>
-        <div class="scroll"></div>      
-      </div>
     </div>
     )
 };
