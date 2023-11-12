3a2bcd7ecb49c661d43fdbb4d3edf454468b0d2e
TriiTonyy1708 <135950372+TriiTonyy1708@users.noreply.github.com>
2023-11-12 17:54:28 +0700

    Added Model

--- client/src/views/LichGiaoHuu.jsx
+++ client/src/views/LichGiaoHuu.jsx
@@ -13,7 +13,6 @@ const LichGiaoHuu = () =>{
               <div class="col-span-1 px-5 text-[28px] font-bold h-[60px] flex flex-col justify-center">Xác nhận</div>
           </div>
 
-
           <div className="mt-3 rounded-[10px] grid grid-cols-7 bg-[#379E13] text-center justify-center py-5 text-[#fff] text-[20px]" >
             <div className="col-span-1 px-5 flex flex-col justify-center">Nguyễn Xuân Lộc</div>
             <div class="col-span-1 px-5 flex flex-col justify-center">1234567890</div>
@@ -21,7 +20,7 @@ const LichGiaoHuu = () =>{
             <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
             <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
             <div className="relative">
-              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3 font-bold" >Tham gia</button>
             </div>
           </div>  
 
@@ -62,7 +61,7 @@ const LichGiaoHuu = () =>{
             <div class="col-span-1 px-5 flex flex-col justify-center">Sân 01</div>
             <div class="col-span-1 px-5 flex flex-col justify-center">T7 31/12/2023<br/>19h00 - 21h00 </div>
             <div className="relative">
-              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3">Tham gia</button>
+              <button class="col-span-1 px-5 bg-[#FFEB37] rounded-[15px] w-[150px] h-[60px] justify-center text-[#000] my-3 ">Tham gia</button>
             </div>
           </div>  
 
